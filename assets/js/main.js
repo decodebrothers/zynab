4740c45b24e1e337868c60941085fb15f70be3d7
decodebrothers <kishanpotter@gmail.com>
2024-04-13 12:41:43 +0530

    new app added

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+$('.screenslider').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 7.5,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 6.2,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5.2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 4.2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1.8,
+          slidesToScroll: 2
+        }
+      }
+    ]
+  });
+
+  AOS.init();
